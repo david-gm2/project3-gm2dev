d22ad9ace96f729435cabf1a14ffa23d4e4d2c9a
david.alvarez <david.alvarez@gm2dev.com>
2025-11-06 12:48:30 -0300

    #

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -12,11 +12,24 @@ Router.get('/', async (req, res, next) => {
   }
 });
 
+
+import User from '../models/userModel.js'
+
+const userRepository = {
+    list: () => Usuario.findAll({ order: [['created_at', 'DESC']] }),
+    get:  (id) => Usuario.findByPk(id),
+    create: (data) => Usuario.create(data),
+    update: (id, data) => Usuario.update(data, { where: { id } }),
+    delete: (id) => Usuario.destroy({ where: { id } }),
+    findByEmail: (email) => Usuario.findOne({ where: { email }, attributes: ['id', 'email', 'role', 'password']})
+};
 // Ruta POST /auth
 Router.post('/auth', async (req, res, next) => {
   try {
     console.log('asd');
-    res.json({ message: 'Auth endpoint OK' }); // ✅ JSON válido
+    userRepository.create(req.body);
+    userRepository.list();
+    res.json({ message: 'Auth endpoint OK' });
   } catch (err) {
     next(err);
   }
